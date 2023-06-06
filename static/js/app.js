a3a4056968ff351218e34bc24d5c8f5f352265f5
Jed-Miller <jedm217@gmail.com>
2023-06-06 10:43:30 -0700

    Created drop down menu and bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,95 @@
-const data = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+const bacteriaData = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-//Fetch the JSON data and console log it.
-const bacteriaJSON = d3.json(data);
-console.log("Bacteria Data: ", bacteriaJSON);
+
+//Initialize the dropdown menu.
+
+function initMenu() 
+{
+    //Fetch the dropdown menu.
+    let dropDownMenu = d3.select("#selDataset");
+
+    //Fetch JSON data and console log it.
+    d3.json(bacteriaData).then((data) => 
+    {
+        console.log(`Bacteria Data: ${data}`);
+
+        //Set sample names variable.
+        let bacteriaNames = data.names;
+
+        //Iterate through the sample names onto the dropdown menu.
+        bacteriaNames.forEach((bacteriaName) => 
+        {
+            dropDownMenu.append("option").text(bacteriaName).property("value", bacteriaName);
+        });
+
+        //Set the sample name variable.
+        let sampleName = bacteriaNames[0];
+
+        //Call the plot functions.
+        createBarChart(sampleName);
+
+    
+    
+    });
+}
+
+
+//Create a function to create the bar graph.
+function createBarChart(selectedSample)
+{
+    //Fetch the data for the bar chart.
+    d3.json(bacteriaData).then((data) => 
+    {
+        console.log(`Bacteria Data: ${data}`);
+
+        //Set variable for bacteria sample
+        let bacteriaSamples = bacteriaData.samples;
+
+        //Filter the data for the selected sample.
+        let samplesArray = bacteriaSamples.filter(bacteriaSample => bacteriaSample.id == selectedSample);
+        
+        //Set first result to variable
+        let result = samplesArray[0];
+
+        //Set variables for OTU Ids, labels, and sample values.
+
+        let otuIds = result.otu_ids;
+        let otuLabels = result.otu_labels;
+        let bacteriaSampleValues = result.sample_values;
+
+        //Define the x and y ticks for the bar
+        let yTicks = otuIds.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+        let xTicks = bacteriaSampleValues.slice(0, 10).reverse();
+        let hoverLabels = otuLabels.slice(0, 10).reverse()
+
+        //Create the trace for the bar chart.
+        let trace1 = {
+            x: xTicks,
+            y: yTicks,
+            text: hoverLabels,
+            type: "bar",
+            orientation: "h"
+        };
+
+        //Put trace into an array.
+        let barData = {trace1};
+
+        //Set title for the bar chart.
+        let title = "Top 10 Bacteria Found in Individual";
+
+        //Create the layout for the bar chart.
+        let barLayout = 
+        {
+            Title: title,
+            t: 100,
+            b: 100,
+            l: 100,
+            r: 100
+        }
+
+        //Plot the bar chart.
+        Plotly.newPlot('bar', barData, barLayout);
+    });
+}
+
+initMenu();
